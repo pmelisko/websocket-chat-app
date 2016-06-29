050f08bb65636c8f2426d4c1c8dc2fc5def1605e
Peter Melisko <pmelisko@gratex.com>
2016-06-29 18:48:05 +0200

    add few improvements, send json to server and back instead of formatted string

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
+/*globals __dirname:false,JSON:false */
+
 // web part
 var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client/index.html');
+	res.sendFile(__dirname + '/client/index.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('http server listening on *:3000');
+	console.log("please browse: http://localhost:3000");
 });
 
-
 // socket part
-
 var PORT = 5555;
 var ws = require("nodejs-websocket");
 var connections = [];
 
 ws.createServer(function (conn) {
-    connections.push(conn);
-    conn.on("text", function (str) {
-        broadcast(str);
-    });
-    conn.on("close", function (/*code, reason*/) {
-    	var idx = connections.indexOf(conn);
-    	~idx && connections.splice(idx, 1);
-        console.log("Connection closed");
-       	broadcast("client closed");
-    });
-}).listen(PORT);
+	connections.push(conn);
+	conn.on("text", function (str) {
+		var data = JSON.parse(str);
 
-function broadcast(message) {
+		var message, style;
+		switch (data.type) {
+			case "open":
+				message = data.user + " has been connected";
+				style = "info";
+				break;
+			case "message":
+				message = data.user + ": " + data.message;
+				break;
+			case "close":
+				message = data.user + " has beed disconnected";
+				style = "info";
+				break;
+			case "rename":
+				message = data.user + " renamed";
+				style = "info";
+				break;
+			case "typing":
+				message = data.user + " is typing";
+				style = "info";
+				break;
+		}
+
+		message && broadcast(message, style);
+	});
+	conn.on("close", function (/*code, reason*/) {
+		var idx = connections.indexOf(conn);
+		~idx && connections.splice(idx, 1);
+		console.log("Connection closed");
+		broadcast("client closed");
+	});
+}).listen(PORT);
+console.log('websocket server listening on ' + PORT);
+
+function broadcast(message, style) {
 	connections.forEach(function(c) {
-		c.sendText(message);
+		c.sendText(JSON.stringify({
+			message : message,
+			style : style
+		}));
 	});
 }
